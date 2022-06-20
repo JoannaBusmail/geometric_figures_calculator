7971ced6b5b53e640a0346ce35578f9a01f0b379
JoannaBusmail <joanna.busmail@gmail.com>
2022-06-20 13:12:22 +0200

    add js class and html elements in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 'use strict';
+// GENERAL ELEMENTS
+const form = document.querySelector('.js_form');
+const resetBtn = document.querySelector('.js_resetBtn');
+// SQUARE ELEMENTS
+const inputSquare = document.querySelector('js_inputSquare');
+const squarePerimeterBtn = document.querySelector('.js_squarePermiterBtn');
+const squarePAreaBtn = document.querySelector('.js_squareAreaBtn');
+//TRIANGLE ELEMENTS
+const inputSideOneTriangle = document.querySelector('.js_inputSideOneTriangle');
+const inputSideTwoTriangle = document.querySelector('.js_inputSideTwoTriangle');
+const inputBaseTriangle = document.querySelector('.js_inputBaseTriangle');
+const trianglePerimeterBtn = document.querySelector('.js_trianglePermiterBtn');
+const triangleAreaBtn = document.querySelector('.js_triangleAreaBtn');
+//CIRCLE ELEMENTS
+const inputRadiusCircle = document.querySelector('.js_inputRadiusCircle');
+const circlePerimeterBtn = document.querySelector('.js_circlePermiterBtn');
+const circleAreaBtn = document.querySelector('.js_circleAreaBtn');
 
-console.log('>> Ready :)');
+// FUNCTIONS
+
+//LISTENERS
